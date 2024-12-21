c2aa448d1a7e8b3de6f24c4b8b841af8a15cb51c
Jordan <jordanpraissman@gmail.com>
2024-12-21 13:17:13 -0500

    Landing page almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import meImage from "./images/Me.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid grid-rows-[auto_1fr] h-screen items-center bg-sky-600">
+      <div className="grid grid-cols-12 w-screen gap-4 items-center">
+        <div className="col-span-4 justify-items-start p-4">
+          <a href={"#"} className="text-2xl text-white font-bold">
+            Jordan Praissman
+          </a>
+        </div>
+        <div className="col-span-8 justify-items-end text-white p-4 pl-8">
+          <div className="space-x-8">
+            <a href={"#"}>Experience</a>
+            <a href={"#"}>Projects</a>
+            <a href={"#"}>Resume</a>
+            <a href={"#"}>Play</a>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="grid grid-cols-12 w-screen gap-4">
+        <div className="col-span-5 justify-items-end">
+          <img src={meImage} className="w-3/5 h-auto rounded-full" />
+        </div>
+        <div className="col-span-7 flex items-center justify-items-start w-4/5 pl-5 text-white">
+          <div className="space-y-3">
+            <h1 className="text-6xl font-bold">Jordan Praissman</h1>
+            <h2 className="text-xl font-bold pt-3">A Bit About Me</h2>
+            <h3 className="text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </h3>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
